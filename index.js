7a8d36a62abb62e746553ede234257755319d0dd
zaquille-oneil <zakglee@gmail.com>
2017-06-27 17:31:24 -0700

    fixed some html and js

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = './favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
